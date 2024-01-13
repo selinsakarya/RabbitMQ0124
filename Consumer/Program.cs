af814a13b9a65952b5bcbaa5a6d7fd06c5da9332
Selin Sakarya <selinsakarya@outlook.com>
2024-01-14 01:09:31 +0300

    .

---
+++ Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+};
+
+using (IConnection connection = factory.CreateConnection())
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("BasicTest", false, false, false, null);
+
+        EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (_, eventArgs) =>
+        {
+            byte[] body = eventArgs.Body.ToArray();
+
+            string message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Received message: {message}");
+        };
+
+        channel.BasicConsume("BasicTest", autoAck: true, consumer);
+    }
+}
+
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672/")
+};
+
+using (IConnection connection = factory.CreateConnection())
+{
+    using (IModel channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("BasicTest", false, false, false);
+
+        string message = "selin";
+
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: "BasicTest",
+            basicProperties: null,
+            body: body);
+
+        Console.WriteLine($"Sent message {message}...");
+    }
+}
+
+Console.ReadLine();
