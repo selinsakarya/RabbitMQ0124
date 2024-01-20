ba4358572c7fe6aff2354a5af2b70deb481af0e7
Selin Sakarya <selinsakarya@outlook.com>
2024-01-21 01:17:42 +0300

    .

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddMvc();
+builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen(c =>
 {
