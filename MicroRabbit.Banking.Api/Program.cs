014e31d4d8d30992c8201fd39cabbfade7f66d91
Selin Sakarya <selinsakarya@outlook.com>
2024-01-21 02:00:12 +0300

    .

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Banking Microservicxe", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Banking Microservice", Version = "v1" });
 });
 var app = builder.Build();
 
